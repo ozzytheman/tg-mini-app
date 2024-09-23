32949d8194d1d67a97bf188c4c690ef4c23e40d7
ozzytheman <103191157+ozzytheman@users.noreply.github.com>
2024-09-24 01:16:45 +0300

    Delete models directory

--- models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const UserSchema = new mongoose.Schema({
-  telegramId: { type: String, required: true, unique: true },
-  username: { type: String, required: true },
-  referrals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-});
-
-export default mongoose.models.User || mongoose.model('User', UserSchema);
